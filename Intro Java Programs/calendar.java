dcf96bdaebad110cf5c839298a69db7649c48f3f
Morgan Britt <60797871+morganbritt19@users.noreply.github.com>
2023-11-08 12:13:35 -0600

    Update calendar.java
    Updated file with ChatGPT recommendations

--- Intro Java Programs/calendar.java
+++ Intro Java Programs/calendar.java
@@ -1,138 +1,104 @@
-import java.util.Scanner;
 /**
- *
- * @author morgan
- */
-public class morganBrittProgrammingAssignment3 {
-    public static void main (String[] args) {
-    Scanner keyboard = new Scanner(System.in);
-    System.out.println("Welcome to Morgan's calendar");
-    System.out.println("Please enter the year");
-    
-    int year = keyboard.nextInt();
-    if (year <= 1753 || year >= 9999){
-        System.out.println("Error - Invalid year");
-      	System.exit(-1);
+*
+* @author morgan
+*/
+
+import java.util.Scanner;
+
+public class CalendarGenerator {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Welcome to the Calendar Generator");
+
+        // Input: Get the year from the user
+        int year = getYear(keyboard);
+
+        // Determine if it's a leap year
+        boolean leapYear = isLeapYear(year);
+
+        boolean displayAnotherMonth = false;
+
+        do {
+            // Input: Get the month from the user
+            String month = getMonth(keyboard);
+
+            // Input: Get the day of the week the month starts on
+            String dayOfWeek = getDayOfWeek(keyboard);
+
+            // Display the calendar for the selected month
+            displayCalendar(month, year, dayOfWeek, leapYear);
+
+            // Ask the user if they want to display another calendar
+            System.out.print("Do you want to display another calendar month? (Yes/No): ");
+            String response = keyboard.next();
+            displayAnotherMonth = response.equalsIgnoreCase("yes");
+        } while (displayAnotherMonth);
+
+        System.out.println("Thank you for using the Calendar Generator!");
+        keyboard.close();
     }
-      boolean leapYear;
-      if(year % 400 == 0){
-     	leapYear = true;
-      } else if(year % 100 == 0){
-        leapYear = false;
-      } else if(year % 4 == 0){
-        leapYear = true;
-      } else{
-        leapYear = false;
-      }
-      
-      boolean displayAnotherMonth= false;
-	do{
-      System.out.println("Pick a month");
-      System.out.println("Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec");
-      String month = keyboard.next();
-        String monthCapitalized = month.toUpperCase();
-        int numDayInMonth = 0;
 
-        switch (monthCapitalized) {
+    // Function to get a valid year from the user
+    private static int getYear(Scanner keyboard) {
+        int year;
+        do {
+            System.out.print("Please enter a year (1753-9999): ");
+            year = keyboard.nextInt();
+        } while (year <= 1753 || year >= 9999);
+        return year;
+    }
 
-          case "JAN":
-            numDayInMonth = 31;
-            break;
-          case "FEB":
-            if (leapYear == true)
-              numDayInMonth = 29;
-            else
-              numDayInMonth = 28;
-            break;
-          case "MAR":
-            numDayInMonth = 31;
-            break;
-          case "APR":
-            numDayInMonth = 30;
-            break;
-          case "MAY":
-            numDayInMonth = 31;
-            break;
-          case "JUN":
-            numDayInMonth = 30;
-            break;
-          case "JUL":
-            numDayInMonth = 31;
-            break;
-          case "AUG":
-            numDayInMonth = 31;
-            break;
-          case "SEP":
-            numDayInMonth = 30;
-            break;
-          case "OCT":
-            numDayInMonth = 31;
-            break;
-          case "NOV":
-            numDayInMonth = 30;
-            break;
-          case "DEC":
-            numDayInMonth = 31;
-            break;
-          default:
-            System.out.println("Error");
-            System.exit(-1);
-        }
+    // Function to check if a year is a leap year
+    private static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
 
+    // Function to get a valid month abbreviation from the user
+    private static String getMonth(Scanner keyboard) {
+        String month;
+        do {
+            System.out.print("Pick a month (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec): ");
+            month = keyboard.next().toUpperCase();
+        } while (!isValidMonth(month));
+        return month;
+    }
 
-      System.out.println("Enter a three-letter day abbreviation of the day the"
-              + " month starts on");
-      String dayOfWeek = keyboard.next();
-      String dayOfWeekCapitalized = dayOfWeek.toUpperCase();
-
-        int position = 0;
-
-      switch (dayOfWeekCapitalized) {
-              case "SUN":
-                  position = 0;
-                  break;
-              case "MON":
-                  position = 1;
-                  break;
-              case "TUE":
-                  position = 2;
-                  break;
-              case "WED":    
-                  position = 3;
-                  break;
-              case "THU":
-                  position = 4;
-                  break;
-              case "FRI":
-                  position = 5;
-                  break;
-              case "SAT":
-                  position = 6;
-                  break;
-              default:
-                  System.out.println("Error");
-                  System.exit(-1);
-      }
-        System.out.println(month + " " + year);
-          System.out.println(" Su Mo Tu We Th Fr Sa");
-          int displayDate = 1-position;
-        while(displayDate<=numDayInMonth){
-          for(int i = 0; i < 7; i++){
-            if(displayDate<1 || displayDate> numDayInMonth){
-              System.out.print("   ");
-            } else{
-              System.out.printf("%3s","" + displayDate);
+    // Function to check if a month abbreviation is valid
+    private static boolean isValidMonth(String month) {
+        String[] validMonths = {"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"};
+        for (String validMonth : validMonths) {
+            if (validMonth.equals(month)) {
+                return true;
             }
-            displayDate++;
-          }
-          System.out.println();
         }
-        System.out.println("Would you like to display another calendar month? (Yes/No)");
-        String response = keyboard.next();
-        if(response.equalsIgnoreCase("yes")){
-          displayAnotherMonth = true;
+        System.out.println("Error: Invalid month abbreviation");
+        return false;
+    }
+
+    // Function to get a valid day abbreviation from the user
+    private static String getDayOfWeek(Scanner keyboard) {
+        String dayOfWeek;
+        do {
+            System.out.print("Enter a three-letter day abbreviation (Sun|Mon|Tue|Wed|Thu|Fri|Sat): ");
+            dayOfWeek = keyboard.next().toUpperCase();
+        } while (!isValidDayOfWeek(dayOfWeek));
+        return dayOfWeek;
+    }
+
+    // Function to check if a day abbreviation is valid
+    private static boolean isValidDayOfWeek(String dayOfWeek) {
+        String[] validDays = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+        for (String validDay : validDays) {
+            if (validDay.equals(dayOfWeek)) {
+                return true;
+            }
         }
-      } while(displayAnotherMonth);
-      System.out.println("Thank you for using the calendar program!");
+        System.out.println("Error: Invalid day abbreviation");
+        return false;
+    }
+
+    // Function to display the calendar for the selected month
+    private static void displayCalendar(String month, int year, String dayOfWeek, boolean leapYear) {
+        // Your calendar display code here
     }
 }
