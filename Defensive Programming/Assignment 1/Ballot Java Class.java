a1999501ad2f7798fc962aa6a7ee47ddeef6a1df
Morgan Britt <60797871+morganbritt19@users.noreply.github.com>
2024-02-23 15:15:16 -0600

    Create Ballot Java Class.java

---
+++ Defensive Programming/Assignment 1/Ballot Java Class.java
@@ -0,0 +1,8 @@
+// Ballot Class that calls the BallotFrame class
+public class Ballot {
+
+    public static void main(String[] args) {
+        // Instantiate the BallotFrame
+        javax.swing.SwingUtilities.invokeLater(BallotFrame::new);
+    }
+}
