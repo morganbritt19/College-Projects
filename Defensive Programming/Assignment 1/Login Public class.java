0d19bb0db0d718c06f2506e5cbe8192588a68e88
Morgan Britt <60797871+morganbritt19@users.noreply.github.com>
2024-02-23 15:14:21 -0600

    Create Login Public class.java

---
+++ Defensive Programming/Assignment 1/Login Public class.java
@@ -0,0 +1,7 @@
+// The following is the public class used to run the the LoginFrame class
+public class Login {
+    public static void main(String[] args) {
+        // Instantiate the LoginFrame
+        javax.swing.SwingUtilities.invokeLater(LoginFrame::new);
+    }
+}
